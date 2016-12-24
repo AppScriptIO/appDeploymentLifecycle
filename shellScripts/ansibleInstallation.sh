46700d6182691c67501bed1c1ebb2d86d0e0d6f6
MyUserInGithub <sfmissive@gmail.com>
2016-12-24 22:22:17 +0200

    Fixed error in shell script.

--- shellScripts/ansibleInstallation.sh
+++ shellScripts/ansibleInstallation.sh
@@ -3,7 +3,8 @@ printf '%s - Installing Ansible...' "$(source $1/$2/printScriptPath.sh $1 $2 $BA
 
 # exits on error - not exactly recommended to use specifically "set -e" for the way it works. read: 
 # http://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
-set -e
+# By default shell will ignore all errors - Windows \r problem for shell scripts causes issues. Therefore its better to keep errors turned off.
+# set -e
 
 # include shell scripts with arguments in case it needs to include sub scripts.
 source $1/$2/commonCommands.sh $1 $2
--- shellScripts/commonCommands.sh
+++ shellScripts/commonCommands.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 printf '%s - Installing commonly used packages...' "$(source $1/$2/printScriptPath.sh $1 $2 $BASH_SOURCE)"
 
+apt-get update
 # software-properties-common allows to add repositories using apt-add-repository.
 apt-get install -y software-properties-common
+apt-get install python-software-properties
