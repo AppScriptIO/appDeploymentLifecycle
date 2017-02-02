d334d0b1a46da5164e3209708924bd1b150abef0
MyUserInGithub <sfmissive@gmail.com>
2017-02-02 06:33:34 +0200

    Updated docker swarmVM commands.

--- dockerSwarmVM_nodeCluster/localContinuousDeploymentDockerCluster_3Nodes.sh
+++ dockerSwarmVM_nodeCluster/localContinuousDeploymentDockerCluster_3Nodes.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 # Create 3 nodes: 1 as jenkins agent, 2 as production-like simulation.
-VM=localContinuousDeploymentNode
-VMCD=localContinuousDeploymentNode
+
+# localContinuousDeploymentNode
+VMCD=CD
+VM=$VMCD
 
 for i in 1 2 3; do
     docker-machine create -d virtualbox $VM-$i
-    # --virtualbox-memory 512
 done
 
 eval $(docker-machine env $VM-1)
-
 docker swarm init --advertise-addr $(docker-machine ip $VM-1)
-
-docker node update --label-add deploymentEnvironment=jenkinsAgent $VM-1
 
 TOKEN=$(docker swarm join-token -q manager)
 for i in 2 3; do
     eval $(docker-machine env $VM-$i)
-
     docker swarm join --token $TOKEN --advertise-addr $(docker-machine ip $VM-$i) $(docker-machine ip $VM-1):2377
-
-    docker node update --label-add deploymentEnvironment=productionLike $VM-$i
 done
+
+# Add labels:
+docker node update --label-add deploymentEnvironment=jenkinsAgent $VM-1
+docker node update --label-add deploymentEnvironment=productionLike $VM-2
+docker node update --label-add deploymentEnvironment=productionLike $VM-3
 
 echo "☕ The swarm test cluster is up and running"
