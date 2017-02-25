ff90367e374c634327d840d6463de683ba24ed5c
MyUserInGithub <sfmissive@gmail.com>
2017-02-25 18:45:49 +0200

    Update gulp module babel.

--- gulp_buildTool/taskModule/babel.js
+++ gulp_buildTool/taskModule/babel.js
@@ -2,8 +2,7 @@
 
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-const gulp = require('gulp')
-
+ 
 export default (sources, destination) => {
     return gulp.src(sources)
         .pipe(babel({
