4cac3f4e3292323e4315aed101532a7723323af1
MyUserInGithub <sfmissive@gmail.com>
2017-02-28 18:10:39 +0200

    Update babel settings instead of default lookup for `.babelrc`

--- gulp_buildTool/taskModule/babel.js
+++ gulp_buildTool/taskModule/babel.js
@@ -5,6 +5,11 @@ let gulp = require('gulp'),
  
 module.exports = (sources, destination) => {
     return gulp.src(sources)
-        .pipe(babel())
+        // NOTE: if babel settings aren't specified. babelrc in the source path would be used, and throw an error because node_modules aren't yet installed.
+        // Therefore, babel settings need to be configured here.
+        .pipe(babel({
+            "presets": ["es2015", "stage-0"],
+            "plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"]
+        }))
         .pipe(gulp.dest(destination));
 };
