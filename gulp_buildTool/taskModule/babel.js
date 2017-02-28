7b148d500b9b73d9653f76d6552135ddd8c4581f
MyUserInGithub <sfmissive@gmail.com>
2017-02-28 18:48:57 +0200

    Update babel.

--- gulp_buildTool/taskModule/babel.js
+++ gulp_buildTool/taskModule/babel.js
@@ -4,7 +4,7 @@ let gulp = require('gulp'),
     babel = require('gulp-babel')
  
 module.exports = (sources, destination, gulpPath) => {
-    return gulp.src(sources, , { cwd: gulpPath })
+    return gulp.src(sources, { cwd: gulpPath })
         // NOTE: if babel settings aren't specified. babelrc in the source path would be used, and throw an error because node_modules aren't yet installed.
         // Therefore, babel settings need to be configured here.
         // Apparently process.cwd is changed in gulp pipeline and babel uses it for node_modules search.
