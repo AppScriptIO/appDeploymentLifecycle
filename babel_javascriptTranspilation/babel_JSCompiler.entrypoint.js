1b9bfaf4a3c046eac1ea5ae965adb90ca6e10b12
MyUserInGithub <sfmissive@gmail.com>
2017-04-13 23:05:35 +0300

    Updated babel plugins path.

--- babel_javascriptTranspilation/babel_JSCompiler.entrypoint.js
+++ babel_javascriptTranspilation/babel_JSCompiler.entrypoint.js
@@ -4,7 +4,8 @@ const path = require('path')
 const filesystem = require('fs')
 const mainModule = require('module')
 function requireJSON(filePath) { return JSON.parse(filesystem.readFileSync(filePath, "utf8")) }; // load non json extension as json.
-const babelDefaultConfig = requireJSON(`${__dirname}/.babelrc`) // load babelrc json.
+// const babelDefaultConfig = requireJSON(`${__dirname}/.babelrc`) // load babelrc json.
+const babelDefaultConfig = require(`${__dirname}/babelConfig.js`) // load babelrc json.
 // babelDefaultConfig.babelrc = false; // don't search for babelrc in transpiled file location.
 
 module.exports = function(nodePath, appFilePath, babelConfig = babelDefaultConfig) {
