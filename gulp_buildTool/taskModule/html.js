ea2d7c6365d7a65a51cb04b208da7365768c93de
MyUserInGithub <sfmissive@gmail.com>
2017-04-13 22:36:13 +0300

    Updated gulp task modules for multiple functions in same task.

--- gulp_buildTool/taskModule/html.js
+++ gulp_buildTool/taskModule/html.js
@@ -1,52 +1,111 @@
-'use strict';
+const gulp = require('gulp')
+const plugins = require('gulp-load-plugins')({ camelize: true })
+const babelInline = require('gulp-babel-inline')
+const babel = require('gulp-babel')
+const merge = require('merge-stream')
+const gulpif = require('gulp-if')
+const uglify = require('gulp-uglify')
+const htmlMinifier = require('gulp-html-minifier')
+const cssSlam = require('css-slam').gulp
+const polyclean = require('polyclean')
+const HtmlSplitter = require('polymer-build').HtmlSplitter
+const sourcesHtmlSplitter = new HtmlSplitter()
 
-let gulp = require('gulp');
-let plugins = require('gulp-load-plugins')({ camelize: true }),
-	babelInline = require('gulp-babel-inline'),
-	babel = require('gulp-babel'),
-	merge = require('merge-stream');
+let ignoreCustomFragments = [ /{%[\s\S]*?%}/, /<%[\s\S]*?%>/, /<\?[\s\S]*?\?>/ ]
+
+function webcomponent(sources, destination) {
+  return gulp.src(sources)
+	.pipe(sourcesHtmlSplitter.split()) // split inline JS & CSS out into individual .js & .css files
+		
+	// Inline CSS
+	.pipe(gulpif(/\.css$/, cssSlam()))
 	
+	// // Inline JAVASCRIPT
+	// .pipe(gulpif(/\.js$/, 
+	// 			babel({
+	// 				"presets": ["es2015"],
+	// 				"plugins": [
+	// 					// "transform-custom-element-classes",
+	// 					"transform-es2015-classes"
+	// 				],
+	// 				"babelrc": false
+	// 			})
+	// ))
+	// .pipe(gulpif(/\.js$/, uglify()))
 
-module.exports = (sources, destination) => {
+	// Inline HTML
+	// .pipe(gulpif(/\.html$/, htmlMinifier()))
+    .pipe(gulpif(/\.html$/, plugins.htmlmin({
+			collapseWhitespace: true,
+			removeComments: true,
+			removeCommentsFromCDATA: true,
+			minifyURLs: true,
+			minifyJS: true,
+			minifyCSS: true, 
+			ignoreCustomFragments: ignoreCustomFragments
+	})))
+
+	.pipe(sourcesHtmlSplitter.rejoin()) // rejoins those files back into their original location
+
+	// .pipe(plugins.plumber())
+	// .pipe(plugins.minifyInline())
+	// .pipe(polyclean.cleanJsComments())
+	// .pipe(polyclean.leftAlignJs())
+	// .pipe(polyclean.uglifyJs())
+	// .pipe(polyclean.cleanCss())
+
+    .pipe(gulp.dest(destination))
+	.pipe(plugins.size({
+		title: 'html task (webcomponent)'
+	}));
+}
+
+function pureHTML(sources, destination) {
+	let hbAttrWrapOpen = /\{\{(#|\^)[^}]+\}\}/;
+	let hbAttrWrapClose = /\{\{\/[^}]+\}\}/;
+	let hbAttrWrapPair = [hbAttrWrapOpen, hbAttrWrapClose];
+	return gulp.src(sources)
+		// .pipe(plugins.plumber())
+		// .pipe(babelInline({
+		// 	"presets": ["es2015"],
+		// 	// "plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"],
+		// 	"babelrc": false
+		// }))
+		// .pipe(babelInline({
+		// 	"presets": ["es2015", "stage-0"],
+		// 	"plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"]
+		// }))
+
+		.pipe(plugins.htmlmin({
+			collapseWhitespace: true,
+			removeComments: true,
+			removeCommentsFromCDATA: true,
+			minifyURLs: true,
+			// minifyJS: true,
+			minifyCSS: true,
+			ignoreCustomFragments: ignoreCustomFragments,
+			// preserveLineBreaks: true
+		}))
+		// .pipe(plugins.minifyInline())
+
+		.pipe(gulp.dest(destination))
+		.pipe(plugins.size({
+			title: 'html task (pureHTML)'
+		}));
+}
+
+module.exports = (sources, destination, type = null) => {
 	return () => {
-		// let html = optimizeHtmlTask( // produces errors.
-		// 	[
-		// 		source('clientSide/assets/**/*.html'),
-		// 		'!'+ source('clientSide/assets/elements/**/*.html'),
-		// 		'!'+ source('clientSide/assets/elements/bower_components/**/*.html'),
-		// 		'!'+ source('clientSide/assets/javascripts/addons_library/Woothemes-FlexSlider2/dynamic-carousel-min-max.html') // Throughs errors.
-		// 	],
-		// 	destination('clientSide/assets')
-		// );
-		var hbAttrWrapOpen = /\{\{(#|\^)[^}]+\}\}/;
-		var hbAttrWrapClose = /\{\{\/[^}]+\}\}/;
-		var hbAttrWrapPair = [hbAttrWrapOpen, hbAttrWrapClose];
-		return gulp.src(sources)
-			// .pipe(plugins.plumber())
-		    // .pipe(babelInline({
-			// 	"presets": ["es2015"],
-			// 	// "plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"],
-			// 	"babelrc": false
-			// }))
-			// .pipe(babelInline({
-			// 	"presets": ["es2015", "stage-0"],
-			// 	"plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"]
-			// }))
-
-			// .pipe(plugins.htmlmin({
-			// 	collapseWhitespace: true,
-			// 	removeComments: true,
-			// 	removeCommentsFromCDATA: true,
-			// 	minifyURLs: true,
-			// 	minifyJS: true,
-			// 	minifyCSS: true,
-			// 	ignoreCustomFragments: [ /{%[\s\S]*?%}/, /<%[\s\S]*?%>/, /<\?[\s\S]*?\?>/ ],
-			// 	preserveLineBreaks: true
-			// }))
-			// .pipe(plugins.minifyInline())
-			.pipe(gulp.dest(destination))
-			.pipe(plugins.size({
-				title: 'optimizeHtmlTask (& Inline JS and CSS)'
-			}));
+		let result;
+		switch (type) {
+			case 'webcomponent':
+				result = webcomponent(sources, destination)
+				break;
+			case 'pureHTML':
+			default:
+				result = pureHTML(sources, destination)
+				break;
+		}
+		return result;
 	}
 };
--- gulp_buildTool/taskModule/javascript.js
+++ gulp_buildTool/taskModule/javascript.js
@@ -2,25 +2,55 @@
 
 let gulp = require('gulp');
 let plugins = require('gulp-load-plugins')({ camelize: true });
-const babel = require('gulp-babel'),
+const babel = require(`${__dirname}/../node_modules/gulp-babel`),
       concat = require('gulp-concat'),
       sourcemaps = require('gulp-sourcemaps'),
       UglifyJS = require("uglify-js");
 
-module.exports = (sources, destination) => {
-  return () => {
+function pureJavascript(sources, destination) {
     return gulp.src(sources)
       .pipe(plugins.plumber())
-      // .pipe(sourcemaps.init())
-      // // .pipe(babel({
-      // //   "presets": ["es2015"],
-      // //   // "plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"],
-      // //   "babelrc": false
-      // // }))
-      // .pipe(sourcemaps.write('.'))
-      // .pipe(plugins.if('*.js', plugins.uglify({
-      //   // preserveComments: 'license'
-      // })))
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        "presets": [`${__dirname}/../node_modules/babel-preset-es2015`],
+        // "plugins": ["babel-plugin-transform-runtime", "babel-plugin-add-module-exports"],
+        "babelrc": false
+      }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(plugins.if('*.js', plugins.uglify({
+        // preserveComments: 'license'
+      })))
       .pipe(gulp.dest(destination));
+}
+
+function babelTranspile(sources, destination, gulpPath) {
+    return gulp.src(sources, { cwd: gulpPath }) // Apparently process.cwd is changed in gulp pipeline and babel uses it for node_modules search.
+    // return gulp.src(sources) 
+        // NOTE: if babel settings aren't specified. babelrc in the source path would be used, and throw an error because node_modules aren't yet installed.
+        // Therefore, babel settings need to be configured here.
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            "presets": [`${__dirname}/../node_modules/babel-preset-es2015`, `${__dirname}/../node_modules/babel-preset-stage-0`],
+            "plugins": [`${__dirname}/../node_modules/babel-plugin-transform-runtime`, `${__dirname}/../node_modules/babel-plugin-add-module-exports`],
+            "babelrc": false
+        }))
+        .pipe(sourcemaps.write('.'))
+        // .pipe(babel())
+        .pipe(gulp.dest(destination));
+}
+
+module.exports = (sources, destination, type = null, gulpPath = null) => {
+  return () => {
+    let result;
+    switch (type) {
+      case 'babelTranspile':
+        result = babelTranspile(sources, destination, gulpPath)
+        break;
+      case 'pureJavascript':
+      default:
+        result = pureJavascript(sources, destination)
+        break;
+    }
+    return result
   }
 };
