8bd194d98b55602da2a95ad8a58a8a4a92c326fa
Dr. Safi <sfmissive@gmail.com>
2017-10-04 17:13:34 +0300

    Changed name, deprecate unused files.

--- babel_javascriptTranspilation/babelConfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "presets": [`${__dirname}/node_modules/babel-preset-es2015`, `${__dirname}/node_modules/babel-preset-stage-0`],
-  "plugins": [`${__dirname}/node_modules/babel-plugin-transform-runtime`, `${__dirname}/node_modules/babel-plugin-add-module-exports`]
-}
