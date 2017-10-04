31f0782bd7bb2773234a44f8b34285a9df27c969
Dr. Safi <sfmissive@gmail.com>
2017-10-04 17:11:30 +0300

    Moved unused modules to deprecated folder for future reference.

---
+++ .deprecatedTools/Vagrant&Ansible/vagrantVMConfigurations/continuesDeployment.vagrantMachine.rb
@@ -0,0 +1,26 @@
+# vagrant client
+Vagrant.configure('2') do |config|
+  # Continues Deployment VMachine.
+  config.vm.define :continuesDeployment do |cd|
+    cd.vm.box = "#{$configVariables['vagrantDefaultImage']}"
+    cd.vm.hostname = "continuesDeployment"
+    cd.vm.network "private_network", ip: "192.168.0.2"
+
+    # Run shell script
+    # cd.vm.provision :shell, path: "./shellScripts/ansibleInstallation.sh"
+    # Ansible Installation - Execute a shell with passing arguments.
+    cd.vm.provision "shell" do |s|
+      # Run shell script - Ansible installation.
+      s.path = "#{$configVariables['shellScriptsFolder']}/ansibleInstallation.sh"
+      # Pass as arguments the path to the VM's configuration files inside the VM. So that each subsequent shell script can know the path to be called from.
+      # $1 is VMConfSynchedFolderOnVM & $2 is shellScriptsFolder.
+      s.args = ["#{$configVariables['VMConfSynchedFolderOnVM']}", "#{$configVariables['shellScriptsFolder']}"]
+    end
+    
+    # Ansible.cfg - Copy ansible.cfg to default folder which Ansible will look for it.
+    cd.vm.provision :shell, inline: "cp #{$configVariables['VMConfSynchedFolderOnVM']}/ansible/ansible.cfg /etc/ansible/ansible.cfg"
+
+    # Ansible Playbook - Execute inline shell command - ansible playbook.
+    cd.vm.provision :shell, inline: "PYTHONUNBUFFERED=1 ansible-playbook #{$configVariables['VMConfSynchedFolderOnVM']}/ansible/continuesDevelopment.yml -c local"
+  end
+end
