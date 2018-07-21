c25accf5c3ec1a974f957ceff723108e352dc126
Dr. Safi <sfmissive@gmail.com>
2018-07-22 02:09:27 +0300

    Added container GUI "portainer" compose file and shell script.

---
+++ deploymentContainer/containerGUI/portainerContainerManagementUI.sh
@@ -0,0 +1,9 @@
+# Portainer is a docker management ui.
+docker-compose \
+        -f ./portainerContainerGUI.dockerCompose.yml \
+        --project-name containerGUI \
+        up -d --force-recreate
+
+docker ps
+
+rundll32 url.dll,FileProtocolHandler http://localhost:9000
