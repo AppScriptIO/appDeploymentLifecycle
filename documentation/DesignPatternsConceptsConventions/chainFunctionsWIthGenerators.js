04b46ceb71f239d020286375dc6ee0e8c974c5eb
Safi <sfmissive@gmail.com>
2019-04-15 00:00:43 +0300

    📔 Update pattern documentation comments.

--- documentation/DesignPatternsConceptsConventions/chainFunctionsWIthGenerators.js
+++ documentation/DesignPatternsConceptsConventions/chainFunctionsWIthGenerators.js
@@ -1,3 +1,8 @@
+/**
+ * Comment for related pattern `switch function` - a function that calls or propagates control to other functions depending on implemenatation key passed to it. 
+ * In the switch pattern the usage of `yield*` won't be possible - `yield*` does not allow for passing arguments to the implicit `next` it calls. i.e. `yield*` will call next on the iterable without arguments, if the iterable was already initialized or uses `function.sent` proposal, the `next` call from `yield*` will not pass arguments, and cannot be controlled.
+ */
+
 function* G() {
   let handControl = Boolean(function.sent)
   const step = [
