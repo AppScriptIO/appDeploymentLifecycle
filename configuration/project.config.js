5a5fc1fb12a982eb5c2a25f62d968468396d39d3
Safi <nassarsafi@gmail.com>
2019-10-23 10:24:38 +0300

    ✒ Update build configuration.

--- configuration/project.config.js
+++ configuration/project.config.js
@@ -1,18 +1,44 @@
 const path = require('path')
 const { script } = require('./script.config.js')
 
-const ownConfiguration = {
+const ownConfig = {
   directory: {
     root: path.resolve(`${__dirname}/..`),
+    get source() {
+      return path.join(ownConfig.directory.root, './source')
+    },
+    get distribution() {
+      return path.join(ownConfig.directory.root, './distribution')
+    },
+    get test() {
+      return path.join(ownConfig.directory.root, './test')
+    },
+    get script() {
+      return path.join(ownConfig.directory.root, './script')
+    },
+    get resource() {
+      return path.join(ownConfig.directory.root, './resource')
+    },
   },
-  script,
+  get script() {
+    return [...script, ...[{ type: 'directory', path: ownConfig.directory.script }]]
+  },
+  entrypoint: {
+    programmaticAPI: './script.js',
+  },
   transpilation: {
     babelConfigKey: 'serverRuntime.BabelConfig.js',
     get babelConfig() {
       const { getBabelConfig } = require('@dependency/javascriptTranspilation')
-      return getBabelConfig(ownConfiguration.transpilation.babelConfigKey, { configType: 'json' })
+      return getBabelConfig(ownConfig.transpilation.babelConfigKey, { configType: 'json' })
     },
+  },
+  build: {
+    get compile() {
+      return [path.relative(ownConfig.directory.root, ownConfig.directory.source), path.relative(ownConfig.directory.root, ownConfig.directory.resource)]
+    },
+    repositoryURL: 'https://github.com/AppScriptIO/appDeploymentLifecycle',
   },
 }
 
-module.exports = ownConfiguration
+module.exports = Object.assign({}, ownConfig)
